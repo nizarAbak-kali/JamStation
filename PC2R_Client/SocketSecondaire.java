f67e356dac1d96bc43aa5102d0182001d2a5f32b
majdBay <majd.bayassi@hotmail.fr>
2015-04-27 18:42:39 +0200

    Update SocketSecondaire.java

--- PC2R_Client/SocketSecondaire.java
+++ PC2R_Client/SocketSecondaire.java
@@ -8,6 +8,9 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
 public class SocketSecondaire extends Thread{
+	
+	
+	
 	public void run(){
 		while(Client.channel2.isClosed())
 			try {
@@ -37,13 +40,35 @@ public class SocketSecondaire extends Thread{
 	            // write the mic data to a stream for use later
 	            //out.write(data, 0, numBytesRead); 
 	            try {
+	            	Client.output2.print("AUDIO_CHUNK/"+Client.tick+"/");
 					Client.output2.write(data);
+					Client.output2.print("/");
+					Client.tick+=4;
+					
+					String response = Client.buff.readLine();
+					
+					if(response.matches("AUDIO_OK"))
+						System.out.println("Server message: " + response);
+					
+					else if(response.matches("AUDIO_KO"))
+						System.err.println("Problème de réception");
+					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 	        }
 	        microphone.close();
+	        String response = null;
+			try {
+				response = Client.buff.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if(response.matches("AUDIO_MIX/.+/"))
+				System.out.println("Bonne réception du buffer audio");
+			
 	    } catch (LineUnavailableException e) {
 	        e.printStackTrace();
 	    } 
